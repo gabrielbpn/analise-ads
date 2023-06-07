ca7d18c1fa487d99aedfef74b992d1474c56e917
gabrielbpn <gabrielbpn0802@gmail.com>
2023-06-07 20:44:09 -0300

    Observando mais parâmetros

--- a.py
+++ a.py
@@ -25,6 +25,128 @@ def analisar_excel(arquivo):
             i+=1
         print(conclusao)
 
+    coluna = df['Campaign Objective']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nObjetivo da campanha:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        if valor_igual == 'Conversions':
+            valor_igual = 'Conversão'
+        conclusao = f'Objetivos das campanhas são esses: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos objetivos diferentes nas campanhas:\n\n'
+        i = 1
+        for valor in valores_diferentes:
+            conclusao += f'{i}: {valor}\n\n'
+            i+=1
+        print(conclusao)
+
+
+    coluna = df['Attribution Spec']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nJanela de Atribuição:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        conclusao = f'Janela de atribuição está igual em todas: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos janelas de atribuição diferentes nas campanhas:\n\n'
+        i = 1
+        for valor in valores_diferentes:
+            if valor == '[{"event_type":"CLICK_THROUGH","window_days":7}]':
+                valor = 'Clique de 7 dias'
+
+            elif valor == '[{"event_type":"CLICK_THROUGH","window_days":7},{"event_type":"VIEW_THROUGH","window_days":1}]':
+                valor = "Clique de 7 dias e atribuição de 1 dia"
+
+            conclusao += f'{i}: {valor}\n\n'
+            i+=1
+        print(conclusao)
+
+    coluna = df['Title']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nTítulo:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        conclusao = f'Títulos iguais: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos títulos diferentes nas campanhas:\n\n'
+        i = 1
+        for valor in valores_diferentes:
+            conclusao += f'{i}: {valor}\n\n'
+            i+=1
+        print(conclusao)
+
+    
+    coluna = df['Body']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nTexto (corpo) do anúncio:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        conclusao = f'Textos iguais: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos textos diferentes nas campanhas:\n\n'
+        i = 1
+        for valor in valores_diferentes:
+            conclusao += f'{i}: {valor}\n\n'
+            i+=1
+        print(conclusao)
+
+    coluna = df['Call to Action']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nCTA do anúncio:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        if valor_igual == 'LEARN_MORE':
+            valor_igual = 'Saiba Mais'
+        conclusao = f'CTA iguais: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos CTAs diferentes nas campanhas:\n\n'
+        i = 1
+        for valor in valores_diferentes:
+            if valor == 'LEAN_MORE':
+                valor == 'Saiba Mais'
+            conclusao += f'{i}: {valor}\n\n'
+            i+=1
+        print(conclusao)
+
+    
+    coluna = df['Instagram Account ID']
+    coluna = coluna.astype(str)
+
+    print("\n---------\nPerfil do Instagram:\n\n")
+
+    if coluna.nunique() == 1:
+        valor_igual = coluna.iloc[0]
+        conclusao = f'Perfis do IG iguais: {valor_igual}'
+        print(conclusao)
+    else:
+        valores_diferentes = coluna.unique()
+        conclusao = f'Opa! Encontramos Perfis diferentes nos anúncios. Vá procurar porque não tem posso te dar o nome deles.\n\n'
+        print(conclusao)
+
+
+
+
     print("\n---------\nEvento de Otimização:\n\n")
 
     coluna = df['Optimized Event']
@@ -70,8 +192,6 @@ def analisar_excel(arquivo):
                 linha = index + 1  # O índice das linhas começa em 0, somamos 1 para ter o número da linha real
                 valor_celula_adjacente = df.at[index, 'Ad Set Name']
                 print(f"Esse conjunto está sem público: {valor_celula_adjacente}")
-
-
 
 
 nome_arquivo = 'exemplo.xlsx'
